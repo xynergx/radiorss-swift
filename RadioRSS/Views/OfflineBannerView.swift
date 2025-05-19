04a15fc874b7e8c1f53ed5f0606bb2c9b6fe3204
Yanis Labbé <211523699+xynergx@users.noreply.github.com>
2025-05-18 20:49:15 -0400

    Add offline banner

---
+++ RadioRSS/Views/OfflineBannerView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct OfflineBannerView: View {
+    @ObservedObject private var monitor = NetworkMonitor.shared
+
+    var body: some View {
+        if !monitor.isConnected {
+            Text("No Internet Connection")
+                .frame(maxWidth: .infinity)
+                .padding(8)
+                .background(Color.red.opacity(0.9))
+                .foregroundColor(.white)
+                .transition(.move(edge: .top))
+        }
+    }
+}
