317838e81bc0593ff185d8b98a97484810c7aaab
Yanis Labbé <211523699+xynergx@users.noreply.github.com>
2025-05-18 21:40:22 -0400

    Enlarge add menu icons

--- RadioRSS/Views/Stations/StationsView.swift
+++ RadioRSS/Views/Stations/StationsView.swift
@@ -53,7 +53,9 @@ struct StationsView: View {
                         Button("Import from File") { showingImportFile = true }
                         Button("Import from URL") { showingImportURL = true }
                         Button("Export JSON") { prepareExport() }
-                    } label: { Image(systemName: "plus") }
+                    } label: { Image(systemName: "plus")
+                            .font(.title3)
+                    }
                 }
             }
             .padding(.bottom, bottomPadding)
